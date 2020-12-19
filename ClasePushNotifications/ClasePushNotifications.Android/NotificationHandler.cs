b14aa3c37d5630080cb3e8b78a57d1faf5290b6e
Yael Chavez <you400@gmail.com>
2020-12-18 18:26:31 -0600

    PushNotification2

---
+++ ClasePushNotifications/ClasePushNotifications.Android/NotificationHandler.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasePushNotifications.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    class NotificationHandler: FirebaseMessagingService
+    {
+        public override void OnMessageReceived(RemoteMessage p0)
+        {
+            base.OnMessageReceived(p0);
+        }
+    } 
+}
